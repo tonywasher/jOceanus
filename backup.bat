f63b8b3f76d09600689def5a3e37319efdfd3f1b
Tony Washer <tonywasher@users.sourceforge.net>
2013-12-15 06:07:09 +0100

    svn315: [#16] Prepare for tag

--- backup.bat
+++ backup.bat
@@ -1,3 +1,3 @@
 @echo off
 if exist ..\source.7z del ..\source.7z
-"\Program Files"\7-Zip\7z a ..\source.7z -x!target/* -x!*/target/* -x!.metadata/* -r *.java *.html *.xml *.xdoc *.apt *.vm *.properties
+"\Program Files"\7-Zip\7z a ..\source.7z -x!target/* -x!*/target/* -x!.metadata/* -r *.java *.html *.xml *.xdoc *.png *.apt *.vm *.properties *.bas
