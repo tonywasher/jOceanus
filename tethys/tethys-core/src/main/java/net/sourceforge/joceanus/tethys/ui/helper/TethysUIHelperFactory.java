aa676f678b08a62bd19acc772756a623c1b21d2f
Tony Washer <tony.washer@yahoo.co.uk>
2024-11-21 08:47:53 +0000

    Update module headers

--- tethys/tethys-core/src/main/java/net/sourceforge/joceanus/tethys/ui/helper/TethysUIHelperFactory.java
+++ tethys/tethys-core/src/main/java/net/sourceforge/joceanus/tethys/ui/helper/TethysUIHelperFactory.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Tethys: Java Utilities
+ * Tethys: GUI Utilities
  * Copyright 2012,2024 Tony Washer
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
