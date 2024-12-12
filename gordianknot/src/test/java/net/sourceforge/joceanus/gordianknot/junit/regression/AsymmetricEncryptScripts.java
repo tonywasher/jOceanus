e4c504b1128c41103832013a0bd96c5b459af9ff
Tony Washer <tony.washer@yahoo.co.uk>
2024-12-12 14:25:06 +0000

    Split out test scripts

---
+++ gordianknot/src/test/java/net/sourceforge/joceanus/gordianknot/junit/regression/AsymmetricEncryptScripts.java
@@ -0,0 +1,180 @@
+/*******************************************************************************
+ * GordianKnot: Security Suite
+ * Copyright 2012,2024 Tony Washer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package net.sourceforge.joceanus.gordianknot.junit.regression;
+
+import net.sourceforge.joceanus.gordianknot.api.base.GordianException;
+import net.sourceforge.joceanus.gordianknot.api.encrypt.GordianEncryptor;
+import net.sourceforge.joceanus.gordianknot.api.encrypt.GordianEncryptorFactory;
+import net.sourceforge.joceanus.gordianknot.api.encrypt.GordianEncryptorSpec;
+import net.sourceforge.joceanus.gordianknot.api.factory.GordianKeyPairFactory;
+import net.sourceforge.joceanus.gordianknot.api.keypair.GordianKeyPair;
+import net.sourceforge.joceanus.gordianknot.impl.core.encrypt.GordianCoreEncryptorFactory;
+import net.sourceforge.joceanus.gordianknot.junit.regression.AsymmetricStore.FactoryEncryptor;
+import net.sourceforge.joceanus.gordianknot.junit.regression.AsymmetricStore.FactoryKeyPairs;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DynamicNode;
+import org.junit.jupiter.api.DynamicTest;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/**
+ * Encrypt scripts.
+ */
+public final class AsymmetricEncryptScripts {
+    /**
+     * Private constructor.
+     */
+    private AsymmetricEncryptScripts() {
+    }
+
+    /**
+     * Create the encryptor test suite for an encryptorSpec.
+     * @param pEncryptor the encryptor
+     * @return the test stream or null
+     */
+    static Stream<DynamicNode> encryptorTests(final FactoryEncryptor pEncryptor) {
+        /* Add self encrypt test */
+        Stream<DynamicNode> myTests = Stream.of(DynamicTest.dynamicTest("SelfEncrypt", () -> checkSelfEncryptor(pEncryptor)));
+
+        /* Add algorithmId test */
+        myTests = Stream.concat(myTests, Stream.of(DynamicTest.dynamicTest("checkAlgId", () -> checkEncryptorAlgId(pEncryptor))));
+
+        /* Check that the partner supports this keySpec*/
+        final GordianKeyPairFactory myTgtAsym = pEncryptor.getOwner().getPartner();
+        if (myTgtAsym != null) {
+            /* Add partner test if the partner supports this encryptore */
+            final GordianEncryptorFactory myTgtEncrypts = pEncryptor.getOwner().getPartner().getEncryptorFactory();
+            if (myTgtEncrypts.validEncryptorSpecForKeyPairSpec(pEncryptor.getOwner().getKeySpec(), pEncryptor.getSpec())) {
+                myTests = Stream.concat(myTests, Stream.of(DynamicTest.dynamicTest("PartnerEncrypt", () -> checkPartnerEncryptor(pEncryptor))));
+            }
+        }
+
+        /* Return the test stream */
+        return myTests;
+    }
+
+    /**
+     * Check Self Encryption.
+     * @param pEncryptor the encryptor
+     * @throws GordianException on error
+     */
+    private static void checkSelfEncryptor(final FactoryEncryptor pEncryptor) throws GordianException {
+        /* Create the data to encrypt */
+        final byte[] mySrc = new byte[AsymmetricTest.TESTLEN];
+
+        /* Access the KeySpec */
+        final GordianEncryptorSpec mySpec = pEncryptor.getSpec();
+        final FactoryKeyPairs myPairs = pEncryptor.getOwner().getKeyPairs();
+        final GordianKeyPair myPair = myPairs.getKeyPair();
+
+        /* Check the encryptor */
+        final GordianEncryptorFactory myEncrypts = pEncryptor.getOwner().getFactory().getEncryptorFactory();
+        final GordianEncryptor mySender = myEncrypts.createEncryptor(mySpec);
+        final GordianEncryptor myReceiver = myEncrypts.createEncryptor(mySpec);
+
+        /* Handle Initialisation */
+        mySender.initForEncrypt(myPair);
+        myReceiver.initForDecrypt(myPair);
+
+        /* Perform the encryption and decryption for all zeros */
+        byte[] myEncrypted = mySender.encrypt(mySrc);
+        byte[] myResult = myReceiver.decrypt(myEncrypted);
+
+        /* Check that the values match */
+        Assertions.assertArrayEquals(mySrc, myResult, "Failed self encryption for all zeros");
+
+        /* Perform the encryption and decryption for all ones */
+        Arrays.fill(mySrc, (byte) 0xFF);
+        myEncrypted = mySender.encrypt(mySrc);
+        myResult = myReceiver.decrypt(myEncrypted);
+
+        /* Check that the values match */
+        Assertions.assertArrayEquals(mySrc, myResult, "Failed self encryption for all ones");
+
+        /* Perform the encryption and decryption for random data */
+        AsymmetricTest.RANDOM.nextBytes(mySrc);
+        myEncrypted = mySender.encrypt(mySrc);
+        myResult = myReceiver.decrypt(myEncrypted);
+
+        /* Check that the values match */
+        Assertions.assertArrayEquals(mySrc, myResult, "Failed self encryption for random data");
+    }
+
+    /**
+     * Check Partner Encryption.
+     * @param pEncryptor the encryptor
+     * @throws GordianException on error
+     */
+    private static void checkPartnerEncryptor(final FactoryEncryptor pEncryptor) throws GordianException {
+        /* Create the data to encrypt */
+        final byte[] mySrc = new byte[AsymmetricTest.TESTLEN];
+
+        /* Access the KeySpec */
+        final GordianEncryptorSpec mySpec = pEncryptor.getSpec();
+        final FactoryKeyPairs myPairs = pEncryptor.getOwner().getKeyPairs();
+        final GordianKeyPair myPair = myPairs.getKeyPair();
+        final GordianKeyPair myPartnerSelf = myPairs.getPartnerSelfKeyPair();
+
+        /* Check the encryptor */
+        final GordianEncryptorFactory mySrcEncrypts = pEncryptor.getOwner().getFactory().getEncryptorFactory();
+        final GordianEncryptorFactory myTgtEncrypts = pEncryptor.getOwner().getPartner().getEncryptorFactory();
+        final GordianEncryptor mySender = mySrcEncrypts.createEncryptor(mySpec);
+        final GordianEncryptor myReceiver = myTgtEncrypts.createEncryptor(mySpec);
+
+        /* Handle Initialisation */
+        mySender.initForEncrypt(myPair);
+        myReceiver.initForDecrypt(myPartnerSelf);
+
+        /* Perform the encryption and decryption on random data */
+        AsymmetricTest.RANDOM.nextBytes(mySrc);
+        final byte[] myEncrypted = mySender.encrypt(mySrc);
+        final byte[] myResult = myReceiver.decrypt(myEncrypted);
+
+        /* Check that the values match */
+        Assertions.assertArrayEquals(mySrc, myResult, "Failed sent encryption");
+
+        /* Create a new target encryption and decrypt at receiver */
+        myReceiver.initForEncrypt(myPartnerSelf);
+        mySender.initForDecrypt(myPair);
+
+        /* Perform the encryption and decryption */
+        final byte[] myEncrypted2 = myReceiver.encrypt(mySrc);
+        final byte[] myResult2 = mySender.decrypt(myEncrypted2);
+
+        /* Check that the values match */
+        Assertions.assertArrayEquals(mySrc, myResult2, "Failed received encryption");
+    }
+
+    /**
+     * Check encryptorAlgId.
+     * @param pEncryptor the encryptor to check
+     */
+    private static void checkEncryptorAlgId(final FactoryEncryptor pEncryptor) {
+        /* Access the factory */
+        final GordianCoreEncryptorFactory myFactory = (GordianCoreEncryptorFactory) pEncryptor.getOwner().getFactory().getEncryptorFactory();
+
+        /* Check that we have an id */
+        final AlgorithmIdentifier myId = myFactory.getIdentifierForSpec(pEncryptor.getSpec());
+        Assertions.assertNotNull(myId,  "Unknown AlgorithmId for " + pEncryptor.getSpec());
+
+        /* Check unique mapping */
+        final GordianEncryptorSpec mySpec = myFactory.getSpecForIdentifier(myId);
+        Assertions.assertEquals(pEncryptor.getSpec(), mySpec, "Invalid mapping for  " + pEncryptor.getSpec());
+    }
+}
---
+++ gordianknot/src/test/java/net/sourceforge/joceanus/gordianknot/junit/regression/SymmetricDigestScripts.java
@@ -0,0 +1,208 @@
+/*******************************************************************************
+ * GordianKnot: Security Suite
+ * Copyright 2012,2024 Tony Washer
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ ******************************************************************************/
+package net.sourceforge.joceanus.gordianknot.junit.regression;
+
+import net.sourceforge.joceanus.gordianknot.api.base.GordianException;
+import net.sourceforge.joceanus.gordianknot.api.digest.GordianDigest;
+import net.sourceforge.joceanus.gordianknot.api.digest.GordianDigestFactory;
+import net.sourceforge.joceanus.gordianknot.api.digest.GordianDigestSpec;
+import net.sourceforge.joceanus.gordianknot.api.factory.GordianFactory;
+import net.sourceforge.joceanus.gordianknot.impl.core.base.GordianCoreFactory;
+import net.sourceforge.joceanus.gordianknot.junit.regression.SymmetricStore.FactoryDigestSpec;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.bouncycastle.util.Arrays;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DynamicContainer;
+import org.junit.jupiter.api.DynamicNode;
+import org.junit.jupiter.api.DynamicTest;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Digest scripts.
+ */
+public class SymmetricDigestScripts {
+    /**
+     * Private constructor.
+     */
+    private SymmetricDigestScripts() {
+    }
+
+    /**
+     * Create the digest test suite for a factory.
+     * @param pFactory the factory
+     * @param pPartner the partner
+     * @return the test stream or null
+     */
+    static Stream<DynamicNode> digestTests(final GordianFactory pFactory,
+                                           final GordianFactory pPartner) {
+        /* Add digest Tests */
+        List<FactoryDigestSpec> myDigests = SymmetricStore.digestProvider(pFactory, pPartner);
+        if (!myDigests.isEmpty()) {
+            Stream<DynamicNode> myTests = myDigests.stream().map(x -> DynamicContainer.dynamicContainer(x.toString(), digestTests(x)));
+            return Stream.of(DynamicContainer.dynamicContainer("Digests", myTests));
+        }
+
+        /* No digest Tests */
+        return null;
+    }
+
+    /**
+     * Create the digest test suite for a digestSpec.
+     * @param pDigestSpec the digestSpec
+     * @return the test stream
+     */
+    private static Stream<DynamicNode> digestTests(final FactoryDigestSpec pDigestSpec) {
+        /* Add profile test */
+        Stream<DynamicNode> myTests = java.util.stream.Stream.of(DynamicTest.dynamicTest("profile", () -> profileDigest(pDigestSpec)));
+
+        /* Add Multi test as long as large data is supported */
+        if (pDigestSpec.getSpec().getDigestType().supportsLargeData()) {
+            myTests = java.util.stream.Stream.concat(myTests, java.util.stream.Stream.of(DynamicTest.dynamicTest("multi", () -> multiDigest(pDigestSpec))));
+        }
+
+        /* Add algorithmId test */
+        myTests = Stream.concat(myTests, Stream.of(DynamicTest.dynamicTest("checkAlgId", () -> checkDigestAlgId(pDigestSpec))));
+
+        /* Add externalId test */
+        myTests = Stream.concat(myTests, Stream.of(DynamicTest.dynamicTest("externalId", () -> SymmetricTest.checkExternalId(pDigestSpec))));
+
+        /* Add partner test if the partner supports this digestSpec */
+        if (pDigestSpec.getPartner() != null) {
+            myTests = Stream.concat(myTests, Stream.of(DynamicTest.dynamicTest("Partner", () -> checkPartnerDigest(pDigestSpec))));
+        }
+
+        /* Return the tests */
+        return myTests;
+    }
+
+    /**
+     * Profile digest.
+     * @param pDigestSpec the digest to profile
+     */
+    private static void profileDigest(final FactoryDigestSpec pDigestSpec) throws GordianException {
+        /* Create the digest */
+        final GordianFactory myFactory = pDigestSpec.getFactory();
+        final GordianDigestSpec mySpec = pDigestSpec.getSpec();
+        final GordianDigestFactory myDigestFactory = myFactory.getDigestFactory();
+        final GordianDigest myDigest = myDigestFactory.createDigest(mySpec);
+
+        /* Check that the digestLength is correct */
+        Assertions.assertEquals(mySpec.getDigestLength().getByteLength(), myDigest.getDigestSize(), "DigestLength incorrect");
+
+        /* Loop 100 times */
+        final byte[] myBytes = getDigestInput(mySpec);
+        final long myStart = System.nanoTime();
+        for (int i = 0; i < SymmetricTest.profileRepeat; i++) {
+            myDigest.update(myBytes);
+            myDigest.finish();
+        }
+
+        /* Calculate elapsed time */
+        long myElapsed = System.nanoTime() - myStart;
+        myElapsed /= SymmetricTest.MILLINANOS * (long) SymmetricTest.profileRepeat;
+        if (SymmetricTest.fullProfiles) {
+            System.out.println(pDigestSpec + ":" + myElapsed);
+        }
+    }
+
+    /**
+     * Multi-call digest.
+     * @param pDigestSpec the digest to profile
+     */
+    private static void multiDigest(final FactoryDigestSpec pDigestSpec) throws GordianException {
+        /* Create the digest */
+        final GordianFactory myFactory = pDigestSpec.getFactory();
+        final GordianDigestSpec mySpec = pDigestSpec.getSpec();
+        final GordianDigestFactory myDigestFactory = myFactory.getDigestFactory();
+        final GordianDigest myDigest = myDigestFactory.createDigest(mySpec);
+
+        /* Check that the digestLength is correct */
+        Assertions.assertEquals(mySpec.getDigestLength().getByteLength(), myDigest.getDigestSize(), "DigestLength incorrect");
+
+        /* Create the digest as a single block */
+        final byte[] myBytes = SymmetricTest.getTestData();
+        myDigest.update(myBytes);
+        final byte[] mySingle = myDigest.finish();
+
+        /* Create the digest as partial blocks */
+        for (int myPos = 0; myPos < SymmetricTest.DATALEN; myPos += SymmetricTest.PARTIALLEN) {
+            final int myLen = Math.min(SymmetricTest.PARTIALLEN, SymmetricTest.DATALEN - myPos);
+            myDigest.update(myBytes, myPos, myLen);
+        }
+        final byte[] myMulti = myDigest.finish();
+
+        /* Check that the results are identical */
+        Assertions.assertArrayEquals(mySingle, myMulti, "Multi-Block and Single-Block results differ");
+    }
+
+    /**
+     * Check partner digest.
+     * @param pDigestSpec the digest to check
+     */
+    private static void checkPartnerDigest(final FactoryDigestSpec pDigestSpec) throws GordianException {
+        /* Create the digests */
+        final GordianFactory myFactory = pDigestSpec.getFactory();
+        final GordianFactory myPartner = pDigestSpec.getPartner();
+        final GordianDigestSpec mySpec = pDigestSpec.getSpec();
+        final GordianDigestFactory myDigestFactory = myFactory.getDigestFactory();
+        final GordianDigest myDigest = myDigestFactory.createDigest(mySpec);
+        final GordianDigestFactory myPartnerFactory = myPartner.getDigestFactory();
+        final GordianDigest myPartnerDigest = myPartnerFactory.createDigest(mySpec);
+
+        /* Calculate digests */
+        final byte[] myBytes = getDigestInput(mySpec);
+        myDigest.update(myBytes);
+        final byte[] myFirst = myDigest.finish();
+        myPartnerDigest.update(myBytes);
+        final byte[] mySecond = myPartnerDigest.finish();
+
+        /* Check that the digests match */
+        Assertions.assertArrayEquals(myFirst, mySecond, "Digest misMatch");
+    }
+
+    /**
+     * Check digestAlgId.
+     * @param pSpec the Spec to check
+     */
+    private static void checkDigestAlgId(final FactoryDigestSpec pSpec) {
+        /* Access the factory */
+        final GordianCoreFactory myFactory = (GordianCoreFactory) pSpec.getFactory();
+
+        /* Check that we have an id */
+        final AlgorithmIdentifier myId = myFactory.getIdentifierForSpec(pSpec.getSpec());
+        Assertions.assertNotNull(myId,  "Unknown AlgorithmId for " + pSpec.getSpec());
+
+        /* Check unique mapping */
+        final GordianDigestSpec mySpec = myFactory.getDigestSpecForIdentifier(myId);
+        Assertions.assertEquals(pSpec.getSpec(), mySpec, "Invalid mapping for  " + pSpec.getSpec());
+    }
+
+    /**
+     * Obtain digest test input.
+     * @param pDigestSpec the digestSpec
+     * @return the input
+     */
+    private static byte[] getDigestInput(final GordianDigestSpec pDigestSpec) {
+        /* Obtain basic input */
+        final byte[] myBytes = "DigestInput".getBytes();
+        return pDigestSpec.getDigestType().supportsLargeData()
+                ? myBytes
+                : Arrays.copyOf(myBytes, pDigestSpec.getDigestState().getLength().getByteLength());
+    }
+}
